9041f386d8e5d1dd14bc2e3748ea8a94520559a5
Alptuğ Yaman <alptugyaman@hotmail.com>
2023-09-03 16:33:12 +0300

    feat: initial commit
    Created with using very_good_cli

---
+++ android/app/src/main/kotlin/com/flutter/clean/architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.clean.architecture.flutter_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_clean_architecture/app.dart';
+import 'package:flutter_clean_architecture/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
